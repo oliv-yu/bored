7b38814a8bb330a97d48ba0f89c45ade340699d4
Olivia Yu <olivyu@gmail.com>
2024-12-14 01:41:12 -0800

    Bored API is down, so replace with simple randomized activity selection
    https://github.com/drewthoennes/Bored-API/issues/83

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -1,19 +1,37 @@
 import React, { useState } from 'react'
 import Card from './shared/Card'
 
+const BORED_ACTIVITIES = [
+	'Learn a new language.',
+	'Code a project you have been putting off.',
+	'Text an old friend.',
+	'Go outside and touch grass.',
+	'Tidy up your room.',
+	'Drink a cup of tea.',
+	'Write in a journal.',
+	'Discover a new podcast.',
+	'Meal prep for the week.',
+	'Try a new recipe.',
+	'Play a video game.',
+	'Practice yoga.',
+	'Bake something sweet.',
+	'Generate AI art.',
+	'Do a jigsaw puzzle.',
+	'Learn how to knit or crochet.',
+	'Read a book someone recommended you.'
+]
+
 function ActivityCard() {
 	const [activity, setActivity] = useState(
 		'Click on Next to get a random activity.'
 	)
 
 	const _handleFetchActivity = () => {
-		fetch('https://www.boredapi.com/api/activity')
-			.then((results) => results.json())
-			.then((data) => {
-				setActivity(data.activity)
-			})
-			.catch(console.log)
+		let randomInt = Math.floor(Math.random() * 17); 
+
+		setActivity(BORED_ACTIVITIES[randomInt]);
 	}
+
 	return (
 		<Card title="DO SOMETHING">
 			<p className="App-card-text card-text">{activity}</p>
